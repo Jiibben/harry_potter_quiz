0978ece103b30089f7ceafb750f224e399c2fa79
Jiibben <allan.burnier@gmail.com>
2021-03-01 16:57:18 +0100

    Update character.js

--- src/components/character.js
+++ src/components/character.js
@@ -3,7 +3,7 @@ import './character.css'
 class Character extends React.Component{
   render(){
     return(<div className="char-wrapper">
-      <img src={this.props.image}></img>
+      <img alt="character" src={this.props.image}></img>
     </div>)
   }
 }
